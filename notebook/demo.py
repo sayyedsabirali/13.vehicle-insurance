b87eae3f9d9e72c194da884bd7c8e90c057a4435
SayyedSabir <sabirali969091@gmail.com>
2025-07-23 18:03:02 +0530

    2-logger-exception-added

---
+++ notebook/demo.py
@@ -0,0 +1,17 @@
+# Access logger code
+# from src.logger import logger
+# logger.info("hi")
+
+
+
+
+
+# Access Exceptioncode
+# from src.exception import MyException
+# from src.logger import logger
+# import sys 
+# try:
+#     a=1/0
+# except Exception as e:
+#     logger.info(e)
+#     raise MyException(e,sys)
