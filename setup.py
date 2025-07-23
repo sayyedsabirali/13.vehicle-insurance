de92fb46626298bb7704c8febe30cf787a428621
SayyedSabir <sabirali969091@gmail.com>
2025-07-23 15:34:49 +0530

    1-Template

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sayyed Sabir Ali",
+    author_email="sabirali969091@gmail.com",
+    packages=find_packages()
+)
