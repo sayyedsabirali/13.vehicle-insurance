d47d1662cd88d297fdd9e6222fffe728461a4494
SayyedSabir <sabirali969091@gmail.com>
2025-07-26 16:04:04 +0530

    7-model-trained-completed

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -32,11 +32,6 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
 
 
 def load_object(file_path: str) -> object:
-    """
-    Returns model/object from project directory.
-    file_path: str location of file to load
-    return: Model/Obj
-    """
     try:
         with open(file_path, "rb") as file_obj:
             obj = dill.load(file_obj)
@@ -45,11 +40,6 @@ def load_object(file_path: str) -> object:
         raise MyException(e, sys) from e
 
 def save_numpy_array_data(file_path: str, array: np.array):
-    """
-    Save numpy array data to file
-    file_path: str location of file to save
-    array: np.array data to save
-    """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
@@ -60,11 +50,6 @@ def save_numpy_array_data(file_path: str, array: np.array):
 
 
 def load_numpy_array_data(file_path: str) -> np.array:
-    """
-    load numpy array data from file
-    file_path: str location of file to load
-    return: np.array data loaded
-    """
     try:
         with open(file_path, 'rb') as file_obj:
             return np.load(file_obj)
@@ -84,22 +69,3 @@ def save_object(file_path: str, obj: object) -> None:
 
     except Exception as e:
         raise MyException(e, sys) from e
-
-
-# def drop_columns(df: DataFrame, cols: list)-> DataFrame:
-
-#     """
-#     drop the columns form a pandas DataFrame
-#     df: pandas DataFrame
-#     cols: list of columns to be dropped
-#     """
-#     logging.info("Entered drop_columns methon of utils")
-
-#     try:
-#         df = df.drop(columns=cols, axis=1)
-
-#         logging.info("Exited the drop_columns method of utils")
-        
-#         return df
-#     except Exception as e:
-#         raise MyException(e, sys) from e
